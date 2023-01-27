f8f3ead1b9f4ca1634fd4e94ae8c79b29c706b6f
lab_502 <gyuho0897@gmail.com>
2023-01-27 12:44:52 +0900

    2023 01 27 before  lecture

--- WinterLecture/src/realCode/Prac11/Prob1.java
+++ WinterLecture/src/realCode/Prac11/Prob1.java
@@ -4,36 +4,63 @@ public class Prob1 {
     public int[] solution(int[] arrival,int[] state){
         int n = arrival.length;
         int[] answer = new int[n];
-        //0
-        Queue<int[]> in = new LinkedList<>();
-        //1
-        Queue<int[]> out = new LinkedList<>();
+        //0 들어옴
+        Queue<Integer> in = new LinkedList<>();
+        //1 나감
+        Queue<Integer> out = new LinkedList<>();
         //사원
-        int pos = 1;
-        int inout;
-        if(state[0] == 0) {
-            answer[0] = arrival[0];
-            inout = 0;
-        }
-        else {
-            answer[0] = arrival[0];
-            inout = 1;
-        }
-        for(int i=arrival[0];;i++){
+        int pos = 0;
+//        int inout;
+//        if(state[0] == 0) {
+//            answer[0] = arrival[0];
+//            inout = 0;
+//        }
+//        else {
+//            answer[0] = arrival[0];
+//            inout = 1;
+//        }
+        int cnt = 0;
+        int inout = state[0];
+        for(int i=0;;i++){
+            System.out.println("second " + i);
+            if(cnt == n) break;
             while(pos < n && arrival[pos] == i){
                 if(state[pos] == 0){
-                    out.offer(new int[]{pos,i});
+                    in.offer(pos);
+                    System.out.println("{" + pos + " " + state[pos] + "}");
+                    //inout = 0;
                 }else if(state[pos] == 1){
-                    in.offer(new int[]{pos,i});
+                    out.offer(pos);
+                    System.out.println("{" + pos + " " + state[pos] + "}");
+                    //inout = 1;
                 }
                 pos++;
             }
-            if(in.peek()[1] <= i && inout == 0){
-                answer[in.peek()[0]] = i;
-            }else if(out.peek()[1] <= i && inout == 1){
-                answer[out.peek()[0]] = i;
+            if(out.isEmpty()) inout = 0;
+            if(in.isEmpty()) inout = 1;
+            if(inout == 0 && !in.isEmpty()){
+                answer[in.peek()] = i;
+                System.out.println(in.peek() + " " + i + " " + inout);
+                in.poll();
+                cnt++;
+                continue;
+            }else if(inout == 1 && !out.isEmpty()){
+                answer[out.peek()] = i;
+                System.out.println(out.peek() + " " + i + " " + inout);
+                out.poll();
+                cnt++;
+                continue;
             }
         }
+        for (int i = 0; i < n; i++) System.out.print(answer[i] + " ");
+        System.out.println();
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Prob1 T = new Prob1();
+        System.out.println(T.solution(new int[]{0, 1, 1, 1, 2, 3, 7},new int[]{1, 0, 0, 1, 0, 1, 1}));
+        System.out.println(T.solution(new int[]{3, 3, 4, 5, 5, 5},new int[]{1, 0, 1, 0, 1, 0}));
+        System.out.println(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10},new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0}));
     }
 }
---
+++ WinterLecture/src/realCode/Prac11/Prob2.java
@@ -0,0 +1,9 @@
+package realCode.Prac11;
+
+public class Prob2 {
+    int solution(int[] s,int[] e){
+        int answer = 0;
+
+        return answer;
+    }
+}
