1063264d3f1b27a50a0609d28e83ec98b9b39fff
lab_502 <gyuho0897@gmail.com>
2023-02-06 13:45:41 +0900

    last commit in com502

--- WinterLecture/src/PracticeCode/Prac5/Prob3.java
+++ WinterLecture/src/PracticeCode/Prac5/Prob3.java
@@ -1,6 +1,21 @@
 package PracticeCode.Prac5;
 import java.util.*;
 public class Prob3 {
+    public int solution1(int[] nums,int k) {
+        int answer = 0;
+        Arrays.sort(nums);
+        int lt = 0;
+        int sum = 0;
+        for(int rt = 0;rt<nums.length;rt++){
+            sum += nums[rt];
+            while(nums[rt] * (rt - lt + 1) > sum + k){
+                sum -= nums[lt];
+                lt++;
+            }
+            answer = Math.max(answer,rt - lt + 1);
+        }
+        return answer;
+    }
     public int solution(int[] nums,int k) {
         int n = nums.length;
         Arrays.sort(nums);
@@ -20,12 +35,12 @@ public class Prob3 {
 
     public static void main(String[] args) {
         Prob3 T = new Prob3();
-        System.out.println(T.solution(new int[]{1, 2, 4, 7}, 5));
-        System.out.println(T.solution(new int[]{5, 7, 8, 2, 9, 6, 3}, 10));
-        System.out.println(T.solution(new int[]{6, 8, 1, 3, 12, 9, 10}, 10));
-        System.out.println(T.solution(new int[]{15, 23, 11, 17, 34, 25, 55, 47, 15}, 30));
-        System.out.println(T.solution(new int[]{9, 7, 12, 33, 21, 23, 33, 22, 34, 22, 33,
+        System.out.println(T.solution1(new int[]{1, 2, 4, 7}, 5));
+        System.out.println(T.solution1(new int[]{5, 7, 8, 2, 9, 6, 3}, 10));
+        System.out.println(T.solution1(new int[]{6, 8, 1, 3, 12, 9, 10}, 10));
+        System.out.println(T.solution1(new int[]{15, 23, 11, 17, 34, 25, 55, 47, 15}, 30));
+        System.out.println(T.solution1(new int[]{9, 7, 12, 33, 21, 23, 33, 22, 34, 22, 33,
                 22}, 33));
-        System.out.println(T.solution(new int[]{-1, 1, 2, 4, 0, -2}, 6));
+        System.out.println(T.solution1(new int[]{-1, 1, 2, 4, 0, -2}, 6));
     }
 }
